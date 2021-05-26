dd8f46891510d9c5d0b0cd044557f3e05e477272
Andrew E Slaughter <slaughter98@gmail.com>
2021-05-26 14:47:08 -0600

    Update MOOSE_DIR detection in moosedocs.py
    (refs #43)

--- doc/moosedocs.py
+++ doc/moosedocs.py
@@ -3,10 +3,10 @@ import sys
 import os
 
 # Locate MOOSE directory
-MOOSE_DIR = os.getenv('MOOSE_DIR', os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..')))
-if not os.path.exists(MOOSE_DIR):
-    MOOSE_DIR = os.path.abspath(os.path.join(os.path.dirname(__name__), '..', '..', 'moose'))
-if not os.path.exists(MOOSE_DIR):
+MOOSE_DIR = os.getenv('MOOSE_DIR', os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'moose')))
+if not os.path.isdir(MOOSE_DIR):
+    MOOSE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'moose'))
+if not os.path.isdir(MOOSE_DIR):
     print('Failed to locate MOOSE, specify the MOOSE_DIR environment variable.')
     sys.exit(1)
 os.environ['MOOSE_DIR'] = MOOSE_DIR
@@ -17,6 +17,7 @@ os.environ['MOOSE_DIR'] = MOOSE_DIR
 
 # Append MOOSE python directory
 MOOSE_PYTHON_DIR = os.path.join(MOOSE_DIR, 'python')
+print(MOOSE_PYTHON_DIR)
 if MOOSE_PYTHON_DIR not in sys.path:
     sys.path.append(MOOSE_PYTHON_DIR)
 
